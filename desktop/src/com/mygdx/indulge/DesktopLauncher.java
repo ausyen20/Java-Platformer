409ea2cecda8ccdfbfb2e555030dc0197b48c283
Minh Tran <33189614+minhtrancccp@users.noreply.github.com>
2022-12-05 16:10:56 +0100

    style: 💄 introduce Google code style
    To provide a consistent format to .java files, I would recommend group members to use the Google java code style. Provided in this commit is an Eclipse style profile named ./CodeStyle.xml that can be voluntarily adopted by members.
    
    Signed-off-by: Minh Tran <33189614+minhtrancccp@users.noreply.github.com>

--- desktop/src/com/mygdx/indulge/DesktopLauncher.java
+++ desktop/src/com/mygdx/indulge/DesktopLauncher.java
@@ -2,14 +2,14 @@ package com.mygdx.indulge;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.mygdx.indulge.Indulge;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setForegroundFPS(60);
-		config.setTitle("Indulge");
-		new Lwjgl3Application(new Indulge(), config);
-	}
+
+  public static void main(String[] arg) {
+    Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+    config.setForegroundFPS(60);
+    config.setTitle("Indulge");
+    new Lwjgl3Application(new Indulge(), config);
+  }
 }
