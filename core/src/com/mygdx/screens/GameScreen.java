d1810ecc2b2e1b8b3e2517673885cbbd80258439
Junior Sarr <Junior Sarr@gmail.com>
2023-01-27 15:21:01 +0100

    Changes to the Lust Level
    - Changed the camera into an orthographic one
    
    - Added the background music
    
    - Added the new layout with the platforms as objects.
    
    - Camera can be moved with the arrow keys to test the level layout

--- core/src/com/mygdx/screens/GameScreen.java
+++ core/src/com/mygdx/screens/GameScreen.java
@@ -1,10 +1,14 @@
 package com.mygdx.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.mygdx.helpers.Constants;
@@ -12,17 +16,31 @@ import com.mygdx.helpers.Constants;
 public abstract class GameScreen implements Screen {
 
     // Screen
-    protected Camera camera;
+    protected OrthographicCamera camera;
     protected Viewport viewport;
 
     // Graphics
     protected SpriteBatch batch;
     protected Texture[] backgrounds;
-    protected Texture layout;
+    protected World world;
+    protected float w = Gdx.graphics.getWidth();
+	protected float h = Gdx.graphics.getHeight();
 
     public GameScreen() {
-        camera = new OrthographicCamera();
+        camera = new OrthographicCamera(16, 9);
+        cameraupdate();
         viewport = new StretchViewport(Constants.WORLD_WIDTH, Constants.WORLD_HEIGHT, camera);
+        world=new World(new Vector2(0,0),false);
+    }
+
+    private void cameraupdate() {
+    	Vector3 position=camera.position;
+    	position.x=0;
+    	position.y=0;
+    	position.z=0;
+     	camera.position.set(new Vector3(position));
+    	camera.setToOrtho(false, position.x, position.y);
+    	camera.update();
     }
 
     @Override
@@ -49,8 +67,9 @@ public abstract class GameScreen implements Screen {
 
     @Override
     public void resize(int width, int height) {
-        // TODO Auto-generated method stub
-        
+        camera.position.set(width/2f, height/2f, 0);
+        viewport.update(width, height, true);
+        batch.setProjectionMatrix(camera.combined);        
     }
 
     @Override
@@ -64,5 +83,10 @@ public abstract class GameScreen implements Screen {
         // TODO Auto-generated method stub
         
     }
+
+    public World getWorld() {
+		// TODO Auto-generated method stub
+		return world;
+	}
     
 }
--- core/src/com/mygdx/screens/LevelLust.java
+++ core/src/com/mygdx/screens/LevelLust.java
@@ -2,10 +2,13 @@ package com.mygdx.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.mygdx.helpers.Levels;
+import com.mygdx.helpers.TileMapHelper;
 import com.mygdx.helpers.Constants;
 import com.mygdx.indulge.Indulge;
 import com.mygdx.objects.Player;
@@ -18,31 +21,53 @@ public class LevelLust extends GameScreen {
 
     // Objects
     private Player player;
+    private Music music;
+    
+    //Tiled Map
+    private OrthogonalTiledMapRenderer orthogonalTiledMapRenderer;
+    private TileMapHelper tileMapHelper=new TileMapHelper(this);
 
     public LevelLust() {
-        // Add background assets
+
         backgrounds = new Texture[3];
         backgrounds[0] = new Texture("backgrounds/lust00.png");
         backgrounds[1] = new Texture("backgrounds/lust01.png");
         backgrounds[2] = new Texture("backgrounds/lust02.png");
         // Set background scrolling speed
         bgMaxScrollingSpeed = (float) (Constants.WORLD_WIDTH) / 5;
-        batch = new SpriteBatch();
+        orthogonalTiledMapRenderer = tileMapHelper.setupMap();
+        this.batch = new SpriteBatch();
+        // Set music
+        music=Gdx.audio.newMusic(Gdx.files.internal("Music/spy-jazz-20925.mp3"));
+        music.setLooping(true);
+        music.play();
+
     }
-
-
+    
     @Override
     public void render(float deltaTime) {
+    	world.step(1/60f,6, 2);
+    	if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+			camera.translate(13, 0, 0);
+		}
+    	camera.update();
+    	batch.setProjectionMatrix(camera.combined);
+    	orthogonalTiledMapRenderer.setView(camera);
+    	
         // Clear screen
         ScreenUtils.clear(0, 0, 0, 1);
         // Change screens with user input
         if (Gdx.input.isKeyPressed(Input.Keys.A)) {
             ((Indulge) Indulge.getInstance()).change_screen(Levels.GLUTTONY);
         }
+        batch.setProjectionMatrix(this.camera.combined);
         batch.begin();
         // Scrolling background
         renderBackground(deltaTime);
         batch.end();
+        camera.update(true);
+        orthogonalTiledMapRenderer.render();
+        
     }
 
     private void renderBackground(float deltaTime) {
@@ -56,12 +81,14 @@ public class LevelLust extends GameScreen {
                 backgroundOffsets[layer] = 0;
             }
             batch.draw(backgrounds[layer], -backgroundOffsets[layer], 0, Constants.ASSET_BACKGROUND_WIDTH, Constants.WORLD_HEIGHT);
-            batch.draw(backgrounds[layer], -backgroundOffsets[layer] + Constants.ASSET_BACKGROUND_WIDTH, 0, Constants.ASSET_BACKGROUND_WIDTH, Constants.WORLD_HEIGHT);
+            for (int i = 1; i < 9; i++ ) {
+                batch.draw(backgrounds[layer], -backgroundOffsets[layer] + i * Constants.ASSET_BACKGROUND_WIDTH, 0, Constants.ASSET_BACKGROUND_WIDTH, Constants.WORLD_HEIGHT);
+
+            }
         }
     }
-
+    
     public void setPlayer(Player player) {
         this.player = player;
-    }
-    
+    } 
 }
