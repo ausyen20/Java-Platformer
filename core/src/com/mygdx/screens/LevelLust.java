fb61c417c4d738d517841cded5143b962a2dd9a5
alor <r.alo@lancaster.ac.uk>
2023-01-28 14:57:51 +0100

    LevelLust now extends GameScreen

--- core/src/com/mygdx/screens/LevelLust.java
+++ core/src/com/mygdx/screens/LevelLust.java
@@ -3,13 +3,9 @@ package com.mygdx.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.audio.Music;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.math.Vector3;
-import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.mygdx.helpers.Levels;
 import com.mygdx.helpers.TileMapHelper;
@@ -17,22 +13,11 @@ import com.mygdx.helpers.Constants;
 import com.mygdx.indulge.Indulge;
 import com.mygdx.objects.Player;
 
-public class LevelLust implements Screen {
-
-    // Screen
-    private OrthographicCamera camera;
-    private Viewport viewport;
-
-    // Graphics
-    private SpriteBatch batch;
-    private Texture[] backgrounds;
-    private World world;
-    float w = Gdx.graphics.getWidth();
-	float h = Gdx.graphics.getHeight();
+public class LevelLust extends GameScreen {
 
     // Timing
     private float[] backgroundOffsets = {0, 0, 0};
-    private float layoutScrollingSpeed;
+    private float bgMaxScrollingSpeed;
 
     // Objects
     private Player player;
@@ -43,15 +28,16 @@ public class LevelLust implements Screen {
     private TileMapHelper tileMapHelper=new TileMapHelper(this);
 
     public LevelLust() {
-
+        // Set background texture
         backgrounds = new Texture[3];
         backgrounds[0] = new Texture("backgrounds/lust00.png");
         backgrounds[1] = new Texture("backgrounds/lust01.png");
         backgrounds[2] = new Texture("backgrounds/lust02.png");
-
-        layoutScrollingSpeed = (float) (Constants.WORLD_WIDTH) / 5;
-        orthogonalTiledMapRenderer=tileMapHelper.setupMap();
+        // Set scrolling speed
+        bgMaxScrollingSpeed = (float) (Constants.WORLD_WIDTH) / 5;
+        orthogonalTiledMapRenderer = tileMapHelper.setupMap();
         this.batch = new SpriteBatch();
+        // Set music
         music = Gdx.audio.newMusic(Gdx.files.internal("Music/spy-jazz-20925.mp3"));
         music.setLooping(true);
         music.play();     
@@ -96,47 +82,14 @@ public class LevelLust implements Screen {
                 backgroundOffsets[layer] = 0;
             }
             batch.draw(backgrounds[layer], -backgroundOffsets[layer], 0, Constants.ASSET_BACKGROUND_WIDTH, Constants.WORLD_HEIGHT);
-            batch.draw(backgrounds[layer], -backgroundOffsets[layer] + Constants.ASSET_BACKGROUND_WIDTH, 0, Constants.ASSET_BACKGROUND_WIDTH, Constants.WORLD_HEIGHT);
-            batch.draw(backgrounds[layer], -backgroundOffsets[layer] + 2 * Constants.ASSET_BACKGROUND_WIDTH, 0, Constants.ASSET_BACKGROUND_WIDTH, Constants.WORLD_HEIGHT);
-            batch.draw(backgrounds[layer], -backgroundOffsets[layer] + 3 * Constants.ASSET_BACKGROUND_WIDTH, 0, Constants.ASSET_BACKGROUND_WIDTH, Constants.WORLD_HEIGHT);
-            batch.draw(backgrounds[layer], -backgroundOffsets[layer] + 4 * Constants.ASSET_BACKGROUND_WIDTH, 0, Constants.ASSET_BACKGROUND_WIDTH, Constants.WORLD_HEIGHT);
-            batch.draw(backgrounds[layer], -backgroundOffsets[layer] + 5 * Constants.ASSET_BACKGROUND_WIDTH, 0, Constants.ASSET_BACKGROUND_WIDTH, Constants.WORLD_HEIGHT);
-            batch.draw(backgrounds[layer], -backgroundOffsets[layer] + 6 * Constants.ASSET_BACKGROUND_WIDTH, 0, Constants.ASSET_BACKGROUND_WIDTH, Constants.WORLD_HEIGHT);
-            batch.draw(backgrounds[layer], -backgroundOffsets[layer] + 7 * Constants.ASSET_BACKGROUND_WIDTH, 0, Constants.ASSET_BACKGROUND_WIDTH, Constants.WORLD_HEIGHT);
-            batch.draw(backgrounds[layer], -backgroundOffsets[layer] + 8 * Constants.ASSET_BACKGROUND_WIDTH, 0, Constants.ASSET_BACKGROUND_WIDTH, Constants.WORLD_HEIGHT);
+            for (int i = 1; i < 9; i++) {
+                batch.draw(backgrounds[layer], -backgroundOffsets[layer] + i * Constants.ASSET_BACKGROUND_WIDTH, 0, Constants.ASSET_BACKGROUND_WIDTH, Constants.WORLD_HEIGHT);
 
+            }
         }
-    }
-    
-    @Override
-    public void resize(int width, int height) {
-    	camera.position.set(width/2f, height/2f, 0);
-        viewport.update(width, height, true);
-        batch.setProjectionMatrix(camera.combined);
-        
-    }
-    
-    @Override
-    public void pause() {
-        
-    }
-    
-    @Override
-    public void resume() {
-        
-    }
-    
-    @Override
-    public void hide() {
-        
     }
     
     public void setPlayer(Player player) {
         this.player = player;
-    }
-
-	public World getWorld() {
-		return world;
-	}
-    
+    }    
 }
