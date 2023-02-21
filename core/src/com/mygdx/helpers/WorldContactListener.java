154de28cddc56dfae1da0619747b0f19d1e95cde
Junior Sarr <Junior Sarr@gmail.com>
2023-02-21 13:33:31 +0100

    Damage on contact
    Character gets damaged when it goes into conatact with a spike

--- core/src/com/mygdx/helpers/WorldContactListener.java
+++ core/src/com/mygdx/helpers/WorldContactListener.java
@@ -7,10 +7,13 @@ import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.Manifold;
 import com.mygdx.objects.Obstacles.Spike;
 import com.mygdx.objects.player.Player;
+import com.mygdx.screens.LevelScreens.GameScreen;
+import com.mygdx.screens.LevelScreens.LevelLust;
 
 public class WorldContactListener implements ContactListener {
 	@Override
 	public void beginContact(Contact contact) {
+		Player Actualplayer=GameScreen.player;
 		Fixture fa = contact.getFixtureA();
 		Fixture fb = contact.getFixtureB();
 		
@@ -22,8 +25,9 @@ public class WorldContactListener implements ContactListener {
 		if(fa.getUserData() instanceof Player || fb.getUserData() instanceof Player) {
 			if(fa.getUserData() instanceof Spike || fb.getUserData() instanceof Spike) { 
 				Player ffa = (Player) fa.getUserData();
+				Actualplayer.damage();
 				//Print I am a player, to indicate there is contact
-				ffa.yell();
+			
 			}
 		}
 	}
