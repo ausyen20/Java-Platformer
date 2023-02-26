019f8af49ae74313b36d02d22e7e7c023843c6a3
Junior Sarr <Junior Sarr@gmail.com>
2023-02-26 21:44:13 +0100

    Created the beta version of the sloth level

--- assets/layouts/Sloth Tileset 3.tsx
+++ assets/layouts/Sloth Tileset 3.tsx
@@ -1,4 +1,15 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.2" name="Sloth Tileset 3" tilewidth="16" tileheight="16" tilecount="323" columns="17">
  <image source="Sloth Layout 3.png" width="272" height="304"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="70" duration="100"/>
+   <frame tileid="74" duration="100"/>
+   <frame tileid="78" duration="100"/>
+   <frame tileid="82" duration="100"/>
+   <frame tileid="78" duration="100"/>
+   <frame tileid="74" duration="100"/>
+   <frame tileid="70" duration="100"/>
+  </animation>
+ </tile>
 </tileset>
